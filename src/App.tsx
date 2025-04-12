f0cf629e588401363ea487635d2c95acc340f08f
Vladimir Vo <me@vldmr.website>
2025-04-12 11:16:43 +0200

    chroe: update project

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
-import {FC} from 'react';
-import styles from './App.module.scss';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 import Emoticon from './components/Emoticon';
 
-const App: FC = () => {
+function App() {
   return (
-    <div className={styles.app}>
-      <code>
-        <pre>Well, let's start coding 👨🏻‍💻👨🏻‍💻👨🏻‍💻</pre>
-      </code>
-
+    <>
+      <div>
+        <a href='https://vite.dev' target='_blank'>
+          <img src={viteLogo} className='logo' alt='Vite logo' />
+        </a>
+        <a href='https://react.dev' target='_blank'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
+        </a>
+      </div>
+      <h1>React + TS</h1>
       <Emoticon />
-    </div>
+      <p className='read-the-docs'>
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   );
-};
+}
 
 export default App;
